10935ad214a8aeeb0e6ddaa43d1f3d26f6000de8
Gaiah <gaiah.a.akopian@gmail.com>
2023-04-03 00:19:37 +0400

    Initial commit for Homework 2

---
+++ src/homework2/Main.java
@@ -0,0 +1,12 @@
+package homework2;
+import homework2.task.*;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int[] arr = new int[]{3,2,1,5,4,51,8,7};
+        Task1.writeIteration(arr);
+        Task2.ParseString();
+    }
+}
